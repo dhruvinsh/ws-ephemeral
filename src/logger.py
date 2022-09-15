f94069e1bf96e44abd0878c29933b757196d8eef
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-15 02:28:26 +0000

    better logging

--- src/logger.py
+++ src/logger.py
@@ -1,23 +1,36 @@
 """
 Module to setup logging
 """
-import os
 import logging
+import os
 
 # logging setup
 DEBUG: bool = bool(os.environ.get("WS_DEBUG"))
 LEVEL: int = logging.DEBUG if DEBUG else logging.INFO
 
-BASIC_FORMAT: str = "%(asctime)s:%(levelname)8s:%(name)s:%(message)s"
-
-# setup basic logging format
-logging.basicConfig(level=LEVEL, format=BASIC_FORMAT)
+BASIC_FORMAT: str = "%(asctime)s:%(levelname)8s:%(name)5s:%(message)s"
 
 
-def create_logger(name: str = __name__, level: int = LEVEL) -> logging.Logger:
-    """create logger based on given name else use default"""
+def create_logger(
+    name: str = __name__, level: int = LEVEL, formatter: str = BASIC_FORMAT
+) -> logging.Logger:
+    """create logger based on given name else use default
+
+    :param name: set logger name, default __name__
+    :parma level: set log level, default INFO
+    :param formatter: set streamHandler format,
+                    default %(asctime)s:%(levelname)8s:%(name)s:%(message)s
+    """
+
+    logger_formatter = logging.Formatter(formatter)
 
     logger: logging.Logger = logging.getLogger(name)
     logger.setLevel(level)
+
+    stream = logging.StreamHandler()
+    stream.setLevel(level)
+    stream.setFormatter(logger_formatter)
+
+    logger.addHandler(stream)
 
     return logger
