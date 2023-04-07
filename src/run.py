d3095e3bfb94a9709f36ced874947ba11b249840
Dhruvin Shah <dhruvin3@gmail.com>
2023-04-06 22:25:59 -0400

    package(s): adding tqdm with stub

--- src/run.py
+++ src/run.py
@@ -3,6 +3,8 @@ Module that run the setup for windscrib's ephemeral port
 """
 import time
 
+from tqdm import trange
+
 from logger import create_logger
 from util import to_seconds
 from ws import reset_ephemeral_port
@@ -15,5 +17,7 @@ logger = create_logger("run")
 while True:
     logger.info("setting the ephemeral port")
     reset_ephemeral_port()
+
     logger.info("going to wait patiently for %s days befor next reset", DAYS)
-    time.sleep(to_seconds(DAYS))
+    for _ in trange(to_seconds(DAYS)):
+        time.sleep(1)
